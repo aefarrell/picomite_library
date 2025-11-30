3d8fd816f6eff54c51adad7545c721994a8b6b2e
allan <75338859+aefarrell@users.noreply.github.com>
2025-11-29 17:04:31 -0700

    Updated template
    Updated the template to use a 3 column layout on each card and link to the source on github.

--- docs/make_docs.py
+++ docs/make_docs.py
@@ -4,13 +4,13 @@ from jinja2 import Environment, FileSystemLoader
 from os import walk
 from os.path import abspath, dirname, isfile, join
 
-def format_date(value, format):
-    if format == "short":
+def format_date(value, fmt):
+    if fmt == "short":
         return value.strftime("%Y-%m-%d")
-    elif format == "long":
+    elif fmt == "long":
         return value.strftime("%B %d, %Y")
     else:
-        return value.strftime(format)
+        return value.strftime(fmt)
 
 def retrieve_cards(directory):
     cards = []
@@ -20,17 +20,21 @@ def retrieve_cards(directory):
                 with open(join(path, filename), 'r') as yml:
                     readme = yaml.safe_load(yml)
                 
-                program = join(path,readme.get('program',None))
-                if isfile(program):
-                    with open(program, 'r') as bas:
+                program_file = readme.get('program',None)
+                program_full = join(path,program_file)
+                if isfile(program_full):
+                    with open(program_full, 'r') as bas:
                         readme['code'] = bas.read()
+                    
+                    relpath = path[len(root_dir):]
+                    readme['url'] = root_url+relpath+'/'+program_file
                 
                 screenshot = join(path,readme.get('screenshot',None))
                 if isfile(screenshot):
                     with open(screenshot, 'rb') as bmp:
                         image = bmp.read()
                     
-                    outfile = readme.get('program')+'.bmp'
+                    outfile = program_file+'.bmp'
                     with open(join(output_dir,'images',outfile), 'wb') as outbmp:
                         outbmp.write(image)
                         readme['screenshot'] = outfile
@@ -47,6 +51,7 @@ template_dir = join(script_dir,'templates')
 
 # walk the directory looking for 'read.me' files and associated '*.bas' files
 root_dir = dirname(script_dir)
+root_url='https://github.com/aefarrell/picomite_library/blob/main'
 categories = [ 'math', 'art', 'utils', 'toys' ]
 cards = { cat:retrieve_cards(join(root_dir,cat)) for cat in categories }
 
