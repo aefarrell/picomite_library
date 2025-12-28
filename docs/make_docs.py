897691bf15db0c6cb278aaa7835894665e718b57
allan <75338859+aefarrell@users.noreply.github.com>
2025-12-27 20:51:25 -0700

    Updated documentation script
    Cleaned up the documentation script a bit by using pathlib

--- docs/make_docs.py
+++ docs/make_docs.py
@@ -1,8 +1,9 @@
-import yaml
-import sys
 from jinja2 import Environment, FileSystemLoader
-from os import walk
-from os.path import abspath, dirname, isfile, join
+from pathlib import Path
+from sys import argv
+
+import yaml
+
 
 def format_date(value, fmt):
     if fmt == "short":
@@ -14,30 +15,26 @@ def format_date(value, fmt):
 
 def retrieve_cards(directory):
     cards = []
-    for path, folders, files in walk(directory):
-        for filename in files:
-            if filename == "read.me":
-                with open(join(path, filename), 'r') as yml:
+    for root, dirs, files in Path.walk(directory):
+        for file in files:
+            if file == "read.me":
+                rme = (root/file).resolve()
+                with rme.open('r') as yml:
                     readme = yaml.safe_load(yml)
-                
-                program_file = readme.get('program',None)
-                program_full = join(path,program_file)
-                if isfile(program_full):
-                    with open(program_full, 'r') as bas:
-                        readme['code'] = bas.read()
+                    program_file = readme.get('program',None)
+                    program_full = (root/program_file).resolve()
+                    if program_full.is_file():
+                        with program_full.open('r') as bas:
+                            readme['code'] = bas.read()
                     
-                    relpath = path[len(root_dir):]
-                    readme['url'] = root_url+relpath+'/'+program_file
+                    relpath = program_full.relative_to(root_dir,walk_up=True).as_posix()
+                    readme['url'] = root_url+relpath
                 
-                screenshot = join(path,readme.get('screenshot',None))
-                if isfile(screenshot):
-                    with open(screenshot, 'rb') as bmp:
-                        image = bmp.read()
-                    
-                    outfile = program_file+'.bmp'
-                    with open(join(output_dir,'images',outfile), 'wb') as outbmp:
-                        outbmp.write(image)
-                        readme['screenshot'] = outfile
+                screenshot = (root/readme.get('screenshot',None)).resolve()
+                if screenshot.is_file():
+                    destination = output_dir/'images'/screenshot.name
+                    screenshot.copy(destination)
+                    readme['screenshot'] = 'images/' + screenshot.name
                 else:
                     readme['screenshot'] = None
                 
@@ -45,15 +42,15 @@ def retrieve_cards(directory):
     return cards
 
 # find important directories
-script_dir = dirname(abspath(sys.argv[0]))
-output_dir = join(script_dir,'output')
-template_dir = join(script_dir,'templates')
+script_dir = Path(argv[0]).parent.resolve()
+output_dir = script_dir/'output'
+template_dir = script_dir/'templates'
 
 # walk the directory looking for 'read.me' files and associated '*.bas' files
-root_dir = dirname(script_dir)
+root_dir = script_dir.parent
 root_url='https://github.com/aefarrell/picomite_library/blob/main'
 categories = [ 'art', 'math', 'toys', 'utils' ]
-cards = { cat:retrieve_cards(join(root_dir,cat)) for cat in categories }
+cards = { cat:retrieve_cards(root_dir/cat) for cat in categories }
 
 # load templates and generate the html files
 environment = Environment(loader=FileSystemLoader(template_dir),
@@ -63,7 +60,7 @@ environment.filters['format_date'] = format_date
 filename = 'index.html'
 index = environment.get_template("index.html")
 content = index.render({ 'cards': cards })
-with open(join(output_dir,filename), mode="w", encoding="utf-8") as fout:
+with open((output_dir/filename), mode="w", encoding="utf-8") as fout:
     fout.write(content)
 
 progfile = environment.get_template("program.html")
@@ -71,7 +68,7 @@ for category in categories:
     for card in cards[category]:
         filename = card['program'] + '.html'
         content = progfile.render({'program': card})
-        with open(join(output_dir,filename), mode="w", encoding="utf-8") as fout:
+        with open((output_dir/filename), mode="w", encoding="utf-8") as fout:
             fout.write(content)
         
     
